1fa0897993eb70a0e191898a58018b596f68ed77
sshhery <shery.h233@gmail.com>
2023-03-21 11:32:00 -0300

    primer repositorio

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { JwtDto } from '../model/jwt-dto';
+import { LoginUsuario } from '../model/login-usuario';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+
+  URL =  'https://protestant-eugine-sshhery.koyeb.app/auth/';
+
+  constructor(private httpClient: HttpClient) { }
+
+ public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+   return this.httpClient.post<any>(this.URL + 'nuevo', nuevoUsuario);
+ }
+
+ public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
+ }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'front-end-e7298',
+    appId: '1:1028990255808:web:62378a16e70441edd8a325',
+    storageBucket: 'front-end-e7298.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDmRsbC0wNPAquwGYRzPM9cspgtPPSnkls',
+    authDomain: 'front-end-e7298.firebaseapp.com',
+    messagingSenderId: '1028990255808',
+  },
+
+  production: false,
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
